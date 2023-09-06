91d38e5b73cdb1d7d4e06be0294659478234e518
rodrigofelicio <rodrigo.felicio@gmail.com>
2023-09-06 08:58:33 -0300

    Structuring Parsing Program Better and Splitting HTML Locators Out of the Python Class

---
+++ webScraping/class_html_parser.py
@@ -0,0 +1,96 @@
+import re
+
+from bs4 import BeautifulSoup
+
+
+ITEM_HTML = '''<html><head></head><body>
+<li class="col-xs-6 col-sm-4 col-md-3 col-lg-3">
+    <article class="product_pod">
+            <div class="image_container">
+                    <a href="catalogue/a-light-in-the-attic_1000/index.html"><img src="media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg" alt="A Light in the Attic" class="thumbnail"></a>
+            </div>
+                <p class="star-rating Three">
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                    <i class="icon-star"></i>
+                </p>
+            <h3><a href="catalogue/a-light-in-the-attic_1000/index.html" title="A Light in the Attic">A Light in the ...</a></h3>
+            <div class="product_price">
+        <p class="price_color">£51.77</p>
+<p class="instock availability">
+    <i class="icon-ok"></i>
+
+        In stock
+
+</p>
+    <form>
+        <button type="submit" class="btn btn-primary btn-block" data-loading-text="Adding...">Add to basket</button>
+    </form>
+            </div>
+    </article>
+</li>
+
+</body></html>
+'''
+
+class ParsedItemLocators:
+    """
+    Locators for an item in the HTML page.
+    
+    This allows us to easily see what our code will be looking at
+    as well as change it quickly if we notice it is now different.
+    """    
+    NAME_LOCATOR = 'article.product_pod h3 a'
+    LINK_LOCATOR = 'article.product_pod h3 a'
+    PRICE_LOCATOR = 'article.product_pod p.price_color' 
+    RATING_LOCATOR = 'article.product_pod p.star-rating' 
+        
+    
+class ParsedItem:
+    """
+    A class to take in an HTML page (or part of it), and
+    find properties of an item in it.
+    """
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser')
+
+    @property
+    def name(self):
+        locator = ParsedItemLocators.NAME_LOCATOR  # CSS locator
+        item_link = self.soup.select_one(locator)
+        item_name = item_link.attrs['title']
+        return item_name
+
+    @property
+    def link(self):
+        locator = ParsedItemLocators.LINK_LOCATOR  # CSS locator
+        item_link = self.soup.select_one(locator).attrs['href']
+        return item_link
+
+    @property
+    def price(self):
+        locator = ParsedItemLocators.PRICE_LOCATOR  # CSS locator
+        item_price = self.soup.select_one(locator).string
+        
+        pattern = '£([0-9]+\.[0-9]+)'
+        matcher = re.search(pattern, item_price)
+        return float(matcher.group(1))
+
+    @property
+    def rating(self):
+        locator = ParsedItemLocators.RATING_LOCATOR
+        star_rating_tag = self.soup.select_one(locator)
+        classes = star_rating_tag.attrs['class']    # ['star-rating', 'Three']
+        rating_classes = [r for r in classes if r != 'star-rating']
+        #this down here is another way of getting the rating classes
+        #rating_classes = filter(lambda x: x != 'star-rating', classes)
+        return rating_classes[0]
+
+
+
+#[ParsedItem(p) for p in soup.find_all('article')]
+item = ParsedItem(ITEM_HTML)
+#print(item.rating())
+print(item.name)
